73f9e8e156207652848f2a876b752bd3566b2913
KMNRao <kmohanarao09@gmail.com>
2024-06-05 13:56:48 +0530

    Shell Commit

---
+++ 02-variables.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Ramesh: Hello Suresh, Good Morning"
+echo "Suresh: Hellow Ramesh, Good Morning"
+echo "Ramesh: How are you Suresh"
+echo "Suresh: I am good Ramesh, what About you"
